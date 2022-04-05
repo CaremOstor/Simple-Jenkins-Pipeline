8a690ef9a8b5bc2e5e84958c3942f843ab4996d1
GavrWampus <gavriilganesj@gmail.com>
2022-04-05 15:39:09 +0200

    Correction scanf havent output problem

--- src/main.c
+++ src/main.c
@@ -4,11 +4,14 @@ int main(){
   int a1, b1, r1, a2, b2, r2, x, y;
 
   printf("\nFirst circle (x y r):");
-  scanf("%d %d %d", &a1, &b1, &r1);
+  msg1 = scanf("%d %d %d", &a1, &b1, &r1);
+  assert(msg);
   printf("\nSecond circle (x y r):");
-  scanf("%d %d %d", &a2, &b2, &r2);
+  msg2 = scanf("%d %d %d", &a2, &b2, &r2);
+  assert(msg2);
   printf("\nThird C (x y):");
-  scanf("%d %d", &cx, &cy);
+  msg3 = scanf("%d %d", &cx, &cy);
+  assert(msg3);
 
   result = logic(a1, b1, r1, a2, b2, r2, x, y);
   printf("result: %d", result);
