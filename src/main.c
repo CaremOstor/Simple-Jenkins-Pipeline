f29dcaa4acb45648a34d10f47fe035ded5acbb68
GavrWampus <gavriilganesj@gmail.com>
2022-04-05 15:42:50 +0200

    Correction for no import <stdio.h> problem

--- src/main.c
+++ src/main.c
@@ -1,16 +1,17 @@
 #include "logic.c"
+#include <stdio.h>
 
 int main(){
   int a1, b1, r1, a2, b2, r2, x, y;
 
   printf("\nFirst circle (x y r):");
-  msg1 = scanf("%d %d %d", &a1, &b1, &r1);
+  int msg1 = scanf("%d %d %d", &a1, &b1, &r1);
   assert(msg);
   printf("\nSecond circle (x y r):");
-  msg2 = scanf("%d %d %d", &a2, &b2, &r2);
+  int msg2 = scanf("%d %d %d", &a2, &b2, &r2);
   assert(msg2);
   printf("\nThird C (x y):");
-  msg3 = scanf("%d %d", &cx, &cy);
+  int msg3 = scanf("%d %d", &cx, &cy);
   assert(msg3);
 
   result = logic(a1, b1, r1, a2, b2, r2, x, y);
