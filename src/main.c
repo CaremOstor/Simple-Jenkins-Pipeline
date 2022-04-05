149cc9e33bd18e41937056d505b2b898a7268d8a
GavrWampus <gavriilganesj@gmail.com>
2022-04-05 15:45:56 +0200

    Correction for no import <assert.h> problem

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include "logic.c"
 #include <stdio.h>
-
+#include <assert.h>
 int main(){
   int a1, b1, r1, a2, b2, r2, x, y;
 
   printf("\nFirst circle (x y r):");
   int msg1 = scanf("%d %d %d", &a1, &b1, &r1);
-  assert(msg);
+  assert(msg1);
   printf("\nSecond circle (x y r):");
   int msg2 = scanf("%d %d %d", &a2, &b2, &r2);
   assert(msg2);
